7f04d305557a15844522b71f1d4b6af4b47626f4
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2024-07-19 00:44:34 +0900

    chore: Update handlers to return JSON-encoded data

--- models/data.go
+++ models/data.go
@@ -3,14 +3,14 @@ package models
 import "time"
 
 var (
-	comment1 = Comment{
+	Comment1 = Comment{
 		CommentID: 1,
 		AritcleID: 1,
 		Message:   "Hello",
 		CreatedAt: time.Now(),
 	}
 
-	comment2 = Comment{
+	Comment2 = Comment{
 		CommentID: 2,
 		AritcleID: 1,
 		Message:   "world",
@@ -25,7 +25,7 @@ var (
 		Contents:    "This is first article",
 		UserName:    "user1",
 		NiceNum:     10,
-		CommentList: []Comment{comment1, comment2},
+		CommentList: []Comment{Comment1, Comment2},
 		CreatedAt:   time.Now(),
 	}
 
