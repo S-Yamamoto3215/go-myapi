fd8f736cfda414b4aa6d6de9b7496c1c3ed91a37
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2024-07-21 16:41:15 +0900

    chore: Refactor InsertArticle function to use SQL prepared statement

--- repositories/articles.go
+++ repositories/articles.go
@@ -1,10 +1,28 @@
 package repositories
 
-import "github.com/S-Yamamoto3215/go-myapi/models"
+import (
+	"database/sql"
 
-// TODO: 新規投稿をDBにインサートする。保存内容と、発生したエラーを返す
-func InsertArticle() (models.Article, error) {
-	return models.Article{}, nil
+	"github.com/S-Yamamoto3215/go-myapi/models"
+)
+
+func InsertArticle(db *sql.DB, article models.Article) (models.Article, error) {
+	const sqlStr = `
+	INSERT INTO articles (title, contents, username, nice, created_at) VALUES
+	(?, ?, ?, 0, NOW())`
+
+	var newArticle models.Article
+	newArticle.Title, newArticle.Contents, newArticle.UserName = article.Title, article.Contents, article.UserName
+
+	result, err := db.Exec(sqlStr, article.Title, article.Contents, article.UserName)
+	if err != nil {
+		return models.Article{}, err
+	}
+
+	id, _ := result.LastInsertId()
+	newArticle.ID = int(id)
+
+	return newArticle, nil
 }
 
 // TODO: 変数 page で指定されたページに表示する投稿一覧をデータベースから取得する関数。取得した記事データと発生したエラーを返す
