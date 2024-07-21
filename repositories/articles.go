9b3884aa1e6712d26e2199f6a0e00f49f5b07d7f
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2024-07-21 17:06:19 +0900

    chore: Refactor SelectArticleDetail function to use SQL query with article ID parameter

--- repositories/articles.go
+++ repositories/articles.go
@@ -52,9 +52,30 @@ func SelectArticleList(db *sql.DB, page int) ([]models.Article, error) {
 	return articleArray, nil
 }
 
-// TODO: 投稿IDを指定して記事データを取得。取得した記事データと発生したエラーを返す
-func SelectArticleDetail() (models.Article, error) {
-	return models.Article{}, nil
+func SelectArticleDetail(db *sql.DB, articleID int) (models.Article, error) {
+	const sqlStr = `
+		SELECT *
+		from articles
+		where article_id = ?;
+	`
+
+	row := db.QueryRow(sqlStr, articleID)
+	if err := row.Err(); err != nil {
+		return models.Article{}, err
+	}
+
+	var article models.Article
+	var createdTime sql.NullTime
+	err := row.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum, &createdTime)
+	if err != nil {
+		return models.Article{}, err
+	}
+
+	if createdTime.Valid {
+		article.CreatedAt = createdTime.Time
+	}
+
+	return article, nil
 }
 
 // TODO: いいねの数をアップデートする。発生したエラーを返す
