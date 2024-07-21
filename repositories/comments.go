7a1ba38f13b01a9cdcc492e999721b446464b7d9
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2024-07-21 17:26:34 +0900

    chore: Refactor InsertComment function to use SQL prepared statement and improve error handling

--- repositories/comments.go
+++ repositories/comments.go
@@ -1,10 +1,29 @@
 package repositories
 
-import "github.com/S-Yamamoto3215/go-myapi/models"
+import (
+	"database/sql"
 
-// TODO: 新規投稿をDBにインサートする。保存内容と、発生したエラーを返す
-func InsertComment() (models.Comment, error) {
-	return models.Comment{}, nil
+	"github.com/S-Yamamoto3215/go-myapi/models"
+)
+
+func InsertComment(db *sql.DB, comment models.Comment) (models.Comment, error) {
+	const sqlStr = `
+		INSERT INTO comments (article_id, message, created_at) VALUES
+		(?, ?, NOW());
+	`
+
+	var newComment models.Comment
+	newComment.AritcleID, newComment.Message = comment.AritcleID, comment.Message
+
+	result, err := db.Exec(sqlStr, comment.AritcleID, comment.Message)
+	if err != nil {
+		return models.Comment{}, err
+	}
+
+	id, _ := result.LastInsertId()
+	newComment.CommentID = int(id)
+
+	return newComment, nil
 }
 
 // TODO: 指定IDの記事についたコメント一覧を取得。取得したコメントデータと発生したエラーを返す
