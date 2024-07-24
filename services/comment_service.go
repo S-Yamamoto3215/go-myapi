00129fa38c4a81fec90cbd251d71c143d7755471
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2024-07-24 10:33:28 +0900

    feat: Add new error codes and handle them in article and comment services

--- services/comment_service.go
+++ services/comment_service.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"github.com/S-Yamamoto3215/go-myapi/apperrors"
 	"github.com/S-Yamamoto3215/go-myapi/models"
 	"github.com/S-Yamamoto3215/go-myapi/repositories"
 )
@@ -8,6 +9,7 @@ import (
 func (s *MyAppService) PostCommentService(comment models.Comment) (models.Comment, error) {
 	newComment, err := repositories.InsertComment(s.db, comment)
 	if err != nil {
+		err = apperrors.InsertDataFailed.Wrap(err, "fail to record data")
 		return models.Comment{}, err
 	}
 
